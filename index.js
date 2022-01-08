32cf201b8be8546ceaace9cf0837e4e88d3e75b5
Vinz2008 <68145293+Vinz2008@users.noreply.github.com>
2022-01-08 13:59:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { exec } = require("child_process");
 const app = express();
-const port = 8000;
+const port = 80;
 
 
 var fs = require('fs');
