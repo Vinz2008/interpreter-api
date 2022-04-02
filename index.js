797cad2ae392a1e556b168fa24ce5b433dd8c673
Vinz2008 <vincentbidarddelanoe@gmail.com>
2022-04-02 12:16:15 +0200

    fixed an ENORMOUS bug that made the api give the output of the last code ran

--- index.js
+++ index.js
@@ -17,13 +17,42 @@ app.use(bodyParser.json())
 
 */
 var output;
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 var fs = require('fs');
 var randomId = "";
 var fileName = "";
 const { stdout, stderr } = require("process");
 const { send } = require('express/lib/response');
 global.output = ""
-function run(code) {
+const run_two =  (code) => {
+    return new Promise(resolve => {
+        randomId = Str.random(50);
+        fileName = "script-" + randomId + ".py"    
+        fs.writeFile(fileName,code, function (err) {
+            if (err) throw err; {
+                console.log("Error " + err);
+            }
+            console.log(`${fileName} created`)
+            exec(`python3 ${fileName}`, (err, stdout, stderr) => {
+                output = stdout
+                console.log(`${fileName} executed`)
+                console.log("output after exec : " + output)
+                fs.unlink(fileName, (err) => {
+                    if (err) {
+                        throw err;
+                    }
+            
+                    console.log(`File ${fileName} is deleted.`);
+                    resolve(output);
+                });
+            });
+        })
+      
+        
+    })
+}
+
+async function run(code) {
     randomId = Str.random(50);
     fileName = "script-" + randomId + ".py"    
     fs.writeFile(fileName,code, function (err) {
@@ -43,6 +72,7 @@ function run(code) {
                 console.log(`File ${fileName} is deleted.`);
             });
     });
+    //console.log(`output out of exec : ${output}`)
     /*exec(`python3 ${fileName}`, (err, stdout, stderr) => {
         output = stdout
         console.log(`${fileName} executed`)
@@ -53,6 +83,7 @@ function run(code) {
 
     //return output
     })
+    //console.log(`output out of write file : ${output}`)
     /*fs.unlink(fileName, (err) => {
         if (err) {
             throw err;
@@ -61,7 +92,6 @@ function run(code) {
         console.log(`File ${fileName} is deleted.`);
     });*/
     /*console.log("output returned : " + output)*/
-    return output
 }
 
 var code = ""
@@ -70,10 +100,13 @@ app.post('/input', async (req, res) => {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");*/
 code = req.body.code;
 console.log(`code: ${code}`)
-var function_output = run(code)
+run_two(code).then((output) => {
+    var function_output = output
+    console.log(`output : ${output}`)
+    console.log(`function_output : ${function_output}`)
+    res.status(200).send(function_output)
+})
 
-console.log(`function_output : ${function_output}`)
-res.status(200).send(function_output)
 //console.log("output :" + output)
 //res.status(200).send(output)
 
