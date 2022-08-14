9f3fd77b1ea7b6cb15616744a70a582ff3f7b7b1
Vincent <vincentbidarddelanoe@gmail.com>
2022-08-14 18:09:28 +0200

    started fixing tests

--- test/test.js
+++ test/test.js
@@ -1,15 +1,16 @@
 const expect = require('chai').expect;
-const mainfile = require('../index');
+const python = require('../python');
+const c = require('../c');
 
 describe('index.js tests', () => {
     describe('index run() Test', () => {
         it('should be hello', () => {
-            const result = mainfile.run("print('hello')")
+            const result = python.run("print('hello')")
             expect(result).to.be.eql('hello');
         });
         it('should be world', () => {
-            const result = mainfile.run("print('world')")
-            expect(result).to.equal(4);
+            const result = c.run("#include <stdio.h> \nint main(){\nprintf('world');\nreturn 0;\n}");
+            expect(result).to.equal('world');
         });
     });
 })
